b6d151d4a88e4c6de3483ad4b0f8cccf8759e619
Lorena Salas <sist2.consultoresmendezcampero@gmail.com>
2016-05-04 16:52:34 -0430

    Tuto 2

---
+++ app/Picnic.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Picnic extends Eloquent {
+    
+    // MASS ASSIGNMENT -------------------------------------------------------
+    // define which attributes are mass assignable (for security)
+    // we only want these 3 attributes able to be filled
+    protected $fillable = array('name', 'taste_level');
+
+    // DEFINE RELATIONSHIPS --------------------------------------------------
+    // define a many to many relationship
+    // also call the linking table
+    public function bears() {
+        return $this->belongsToMany('Bear', 'bears_picnics', 'picnic_id', 'bear_id');
+    }
+
+}
